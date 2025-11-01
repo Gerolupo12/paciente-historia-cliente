800397a0364285b8b9d3df104636eee69c455f66
Cristian Lahoz <lahozcristian@gmail.com>
2025-11-01 00:03:46 -0300

    Refactorizar el método main para crear una instancia de AppMenu y ejecutar su método run

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,9 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        AppMenu.main(args);
+
+        AppMenu app = new AppMenu();
+        app.run();
     }
 
 }
