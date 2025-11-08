23a88dbbdfac38184a7351d7401571b0339dc59e
AriMaldo19 <arimaldo19@gmail.com>
2025-11-08 15:59:01 -0300

    Agregados comentarios Javadoc al paquete exceptions

--- src/exceptions/ServiceException.java
+++ src/exceptions/ServiceException.java
@@ -1,5 +1,27 @@
 package exceptions;
-
+/**
+ * Excepción general para errores en la capa de servicio.
+ * <p>
+ * Se utiliza para encapsular errores no controlados o
+ * excepciones técnicas que ocurren durante la ejecución
+ * de una operación de negocio (por ejemplo, errores de base de datos).
+ * </p>
+ *
+ * <p>
+ * A diferencia de {@link ValidationException}, esta clase representa
+ * errores internos o transaccionales, no imputables al usuario.
+ * </p>
+ *
+ * <h3>Ejemplos:</h3>
+ * <ul>
+ *   <li>Fallo en la conexión a la base de datos.</li>
+ *   <li>Error durante un commit o rollback transaccional.</li>
+ * </ul>
+ *
+ * @author alpha
+ * @see service.PacienteService
+ * @see service.HistoriaClinicaService
+ */
 public class ServiceException extends Exception {
     public ServiceException(String message) {
         super(message);
